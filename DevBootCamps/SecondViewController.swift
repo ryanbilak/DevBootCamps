beb2e4c50a92d0261904c3014d1938e06dd888c7
Ryan Bilak <ryan@beachambilak.com>
2016-06-25 14:09:45 -0400

    Initial Commit

---
+++ DevBootCamps/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DevBootCamps
+//
+//  Created by Ryan Bilak on 6/25/16.
+//  Copyright © 2016 beachambilak. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
